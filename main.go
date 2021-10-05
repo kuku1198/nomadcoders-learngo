d3044bf1c5a482d49d421c51286b11f45a328145
어영규 <kuku1198@gmail.com>
2021-10-05 19:20:47 +0900

    #1.3 Functions part One

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	name := "nico"
-	name = "lynn"
-	fmt.Println(name)
+	fmt.Println(multiply(2, 2))
+
+	// totalLength, upperName := lenAndUpper("nico")
+	// fmt.Println(totalLength, upperName)
+
+	totalLength, _ := lenAndUpper("nico")
+	fmt.Println(totalLength)
+
+	repeatMe("nico", "lynn", "dal")
 }
