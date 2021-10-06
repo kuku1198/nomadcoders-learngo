8971b6b449889d07b55e8aeedf8847a5016e8b1c
어영규 <kuku1198@gmail.com>
2021-10-06 18:21:10 +0900

    #1.5 for, range, ...args

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I'm done")
+func superAdd(numbers ...int) int {
+	// for index, number := range numbers {
+	// 	fmt.Println(index, number)
+	// }
 
-	length = len(name)
-	uppercase = strings.ToUpper(name)
+	// for i := 0; i < len(numbers); i++ {
+	// 	fmt.Println(numbers[i])
+	// }
 
-	return
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+
+	return total
 }
 
 func main() {
-	totalLength, up := lenAndUpper("nico")
-	fmt.Println(totalLength, up)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
