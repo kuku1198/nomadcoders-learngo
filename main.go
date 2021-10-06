e23f1ddeb1d58390a853a067a912e0680a70144c
어영규 <kuku1198@gmail.com>
2021-10-06 18:57:42 +0900

    #1.6 If with a Twist

--- main.go
+++ main.go
@@ -2,24 +2,14 @@ package main
 
 import "fmt"
 
-func superAdd(numbers ...int) int {
-	// for index, number := range numbers {
-	// 	fmt.Println(index, number)
-	// }
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	} 
 
-	// for i := 0; i < len(numbers); i++ {
-	// 	fmt.Println(numbers[i])
-	// }
-
-	total := 0
-	for _, number := range numbers {
-		total += number
-	}
-
-	return total
+	return true
 }
 
 func main() {
-	result := superAdd(1, 2, 3, 4, 5, 6)
-	fmt.Println(result)
+	fmt.Println(canIDrink(16))
 }
