8bbe0c3d26dbdd5dbed20cd646b50096238d9688
어영규 <kuku1198@gmail.com>
2021-10-11 18:36:26 +0900

    #3.0 hitURL

--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/kuku1198/nomadcoders-learngo/mydict"
+	"net/http"
 )
 
-func main() {
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
+var errRequestFailed = errors.New("Request failed")
 
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(word)
+func main() {
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
+
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println(url)
+	resp, err := http.Get(url)
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+
+	return nil
 }
